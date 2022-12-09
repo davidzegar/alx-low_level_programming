ce0ed2ef862448234b2d518c685244811166bdb4
davidzegar <zegardavid@yahoo.com>
2022-12-08 19:17:40 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
-#include <stdio.h>
-/*
- *  main - print a line of code using puts
- *
- *  Return: 0
- */
-int  main(void)
-{ 
-	puts("\"Programming is like building a multilingual puzzle");
+#include <stdio.h>                                                                                                                      
+/**                                                                                                                                     
+ * main - Entry                                                                                                                         
+ * Return: Always 0                                                                                                                     
+ */ 
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-
