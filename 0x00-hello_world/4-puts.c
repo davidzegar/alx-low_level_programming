d0d9bb18debf478e4e4c10a9156d8afb5b0a9170
davidzegar <zegardavid@yahoo.com>
2022-12-08 19:39:04 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>                                                                                                                      
 /**                                                                                                                                     
- * main -print a line of code using puts                                                                                                                      
- * Return: 0                                                                                                                     
- */ 
+ *  main -print a line of code using puts
+ *
+ *  Return: 0                                                                                                                     
+*/
 int main(void)
 {
-	printf("\"Programming is like building a multilingual puzzle
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
