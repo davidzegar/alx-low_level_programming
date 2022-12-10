2c433f012500ebd9e6ad906da766a0ebcab9ea2b
davidzegar <zegardavid@yahoo.com>
2022-12-10 13:22:48 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
-#include <stdio.h>                                                                                                                      
-/**                                                                                                                                     
- *  main -print a line of code using puts
+#include <stdio.h> 
+/*
+ * main - print a line of codes using puts
  *
- *  Return: 0                                                                                                                     
+ * Return: 0
 */
 int main(void)
 {
