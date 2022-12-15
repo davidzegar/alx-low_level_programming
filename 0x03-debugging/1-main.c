e3055ecc02fe56b2ed3f02ae134b05947e9a8aa0
davidzegar <zegardavid@yahoo.com>
2022-12-15 13:20:22 -0800

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int i;
 
-	printf("infinite loop incoming:(\n");
+	printf("Infinite loop incoming:(\n");
 
 	i = 0;
 	/*
@@ -17,7 +17,7 @@ int main(void)
 	 * putchar(i);
 	 * }
 	 */
-	printf("infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
