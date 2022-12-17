c4e5c76539e390614e0fcfcd164e888e231dbe09
davidzegar <zegardavid@yahoo.com>
2022-12-16 16:37:30 -0800

    Collaboration is multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,22 +1,12 @@
 #include "main.h"
 
 /**
- * print_triangle - print triangle of variable length
- * @size: triangle height
- * Return: return nothing
+ * mul - returns product of a & b
+ * @a: first integer
+ * @b: second integer
+ * Return: product of a & b
  */
-void print_triangle(int size)
+int mul(int a, int b)
 {
-	int i, a;
-
-	a = size;
-	if (size <= 0)
-		_putchar('\n');
-	while (size > 0)
-	{
-		for (i = 0; i < a; i++)
-			_putchar((i < size - 1) ? ' ' : '#');
-		_putchar('\n');
-		size--;
-	}
+	return (a * b);
 }
