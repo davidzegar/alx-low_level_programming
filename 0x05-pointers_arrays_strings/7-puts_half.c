0b7fff7fcd55a6a66a382b2218acb9497ff1ff74
davidzegar <zegardavid@yahoo.com>
2022-12-19 04:07:26 -0800

    Winning is only half of it. Having fun is the other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,7 +5,7 @@
  * puts_half - a function that prints half of a string
  * if odd len, n = (length_of_the_string - 1) / 2
  * @str: input
- * Return: half of input
+ * Return: 0
  */
 void puts_half(char *str)
 {
