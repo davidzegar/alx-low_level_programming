089e3b6ebe2ac1b7ec4761c2705ad5aa2883050e
davidzegar <zegardavid@yahoo.com>
2022-12-23 12:45:48 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,7 +5,6 @@
  * @str: a string of length to be returned
  * Return: returns the length of a string
  */
-
 int _strlen(char *str)
 {
 	int length = 0;
@@ -16,4 +15,27 @@ int _strlen(char *str)
 		length++;
 	}
 	return (length);
+}
+
+/**
+ * _strcat - concatinates two strings
+ * @dest: destination pointer
+ * @src: pointer to a string
+ * Return: concatinated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *cat = dest + _strlen(dest);
+	int length =  _strlen(dest) + _strlen(src);
+
+	while (*src)
+	{
+		*cat += *src;
+		src++;
+		cat++;
+	}
+	*cat += '\0';
+	cat -= (length);
+	*dest = *cat;
+	return (cat);
 }
